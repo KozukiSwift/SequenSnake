d687bc04f3c52f04d8c25a7bd7ee2de9e489e605
Tomasz Koziol <koziol.tomek@gmail.com>
2022-05-25 15:55:25 +0200

    Moved distanceBetween fuction to newly created gameFunctions.lua. Renamed game.lua to gameVariables.lua

--- cherry.lua
+++ cherry.lua
@@ -7,10 +7,6 @@ function cherry:spawn(size, borderLeft, borderRight, borderTop, borderBottom)
     self.y = math.random(borderTop / size, math.floor(borderBottom / size - 1)) * size
 end
 
-function distanceBetween(x1, y1, x2, y2)
-    return math.sqrt((x2 - x1)^2 + (y2 - y1)^2)
-end
-
 function cherry:draw(size, r, g, b)
     love.graphics.setColor(r, g, b)
     love.graphics.rectangle('fill', self.x, self.y, size, size)
