f3ac3b41ebb713e5014898fefa4d56499f2c0568
Tomasz Koziol <koziol.tomek@gmail.com>
2022-05-26 09:36:27 +0200

    Moved rest of love.update code to playGame function i gameFunctions.lua

--- main.lua
+++ main.lua
@@ -16,36 +16,7 @@ function love.load()
 end
 
 function love.update(dt)
-    game.timer = game.timer + dt
-    game.moveTimer = game.moveTimer + dt * cfg.speed / 60
-    
-    if game.moveTimer > 1 and game.state == 1 then
-        if snake.justAte == false then
-            snake:move(snake.position)
-            game.moveTimer = 0
-        elseif snake.justAte == true then
-            local snakePart = {}
-            snakePart.x = snake.position[#snake.position].x
-            snakePart.y = snake.position[#snake.position].y 
-
-            snake:move(snake.position)
-            
-            table.insert(snake.position, snakePart)
-
-            game.moveTimer = 0
-            snake.justAte = false
-        end
-    end
-
-    -- checking if snake ate the cherry (+ spawning new cherry)
-    if distanceBetween(snake.position[1].x, snake.position[1].y, cherry.x, cherry.y) < cfg.size / 2 then
-        cherry:spawn(cfg.size, cfg.border.left, cfg.border.right, cfg.border.top, cfg.border.bottom)
-        game.score = game.score + 1
-        snake.justAte = true
-    end
-
-    -- checking if snake hit the boarder
-    checkBorderHit()
+    playGame(dt)
 end
 
 function love.draw()
